6cb64eee7850055f061cb339fdf937a73a8ff517
senhoritaassis <47746417+senhoritaassis@users.noreply.github.com>
2019-03-11 10:53:38 -0300

    Add files via upload
    lista 3

---
+++ Ex1.php
@@ -0,0 +1,7 @@
+
+<form method="POST" action="resultado-soma.php">
+    Numero1 <input type ="text" name="Numero1">
+    Numero2 <input type ="text" name="Numero2">
+    <button type="submit">CLICK!</button>
+</form>
+
---
+++ resultado-soma.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$Numero1 = $_POST["Numero1"];
+$Numero2 = $_POST["Numero2"];
+
+$soma = $Numero1+$Numero2;
+echo "a soma é : $soma";
